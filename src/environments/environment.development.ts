f714ab9bf560034c516a53e08e6fce8e458375ba
Henry Reaño <henry29.2004@hotmail.com>
2025-05-01 16:44:42 -0500

    chore: initial commit

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  AICApiBaseUrl: 'https://api.artic.edu/api/v1',
+  exhibitionsEndpointPath: '/exhibitions?fields=id,title,short_description,status,web_url,image_url,gallery',
+  logoProviderApiBaseUrl: 'https://logo.clearbit.com/'
+};
