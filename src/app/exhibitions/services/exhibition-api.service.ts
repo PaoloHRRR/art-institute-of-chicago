ab7664474d9a639a68e263f43b4df83f317e0303
Henry Reaño <henry29.2004@hotmail.com>
2025-05-01 17:51:37 -0500

    bugfix: fix errors in exhibition api service

--- src/app/exhibitions/services/exhibition-api.service.ts
+++ src/app/exhibitions/services/exhibition-api.service.ts
@@ -1,14 +1,25 @@
 import {Injectable} from '@angular/core';
 import {environment} from '../../../environments/environment.development';
 import {Exhibition} from '../model/exhibition.entity';
+import {ExhibitionResponse} from './exhibitions.response';
+import {ExhibitionAssembler} from './exhibition.assembler';
+import {HttpClient} from '@angular/common/http';
+import {map, Observable} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ExhibitionApiService {
-  baseUrl = environment.AICApiBaseUrl;
+  private baseUrl = environment.AICApiBaseUrl;
+  private exhibitionEndpoint = environment.exhibitionsEndpointPath;
 
-  constructor() {
+  constructor(private http: HttpClient) {}
+
+  getExhibitions(): Observable<Exhibition[]> {
+    return this.http.get<ExhibitionResponse>(`${this.baseUrl}${this.exhibitionEndpoint}`, {
+      params: {}
+    }).pipe(
+      map(response => ExhibitionAssembler.toEntitiesFromResponse(response))
+    );
   }
-
 }
