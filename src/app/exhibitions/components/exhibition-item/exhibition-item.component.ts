232767e0ac39db8f686efe094f012927d9844019
Henry Reaño <henry29.2004@hotmail.com>
2025-05-01 19:33:42 -0500

    feat: add styles in exhibition item component

--- src/app/exhibitions/components/exhibition-item/exhibition-item.component.ts
+++ src/app/exhibitions/components/exhibition-item/exhibition-item.component.ts
@@ -2,10 +2,11 @@ import {Component, EventEmitter, Input, Output} from '@angular/core';
 import {Exhibition} from '../../model/exhibition.entity';
 import {MatCardModule} from '@angular/material/card';
 import {TranslatePipe} from '@ngx-translate/core';
+import {DatePipe} from '@angular/common';
 
 @Component({
   selector: 'app-exhibition-item',
-  imports: [MatCardModule, TranslatePipe],
+  imports: [MatCardModule, TranslatePipe, DatePipe],
   templateUrl: './exhibition-item.component.html',
   styleUrl: './exhibition-item.component.css'
 })
