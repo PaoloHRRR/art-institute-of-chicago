b3e93e915ff6ad1f6e5d888bbda23163374f0659
Henry Reaño <henry29.2004@hotmail.com>
2025-05-01 19:43:33 -0500

    feat: add placeholder image if exhibition image is null

--- src/app/exhibitions/services/exhibition.assembler.ts
+++ src/app/exhibitions/services/exhibition.assembler.ts
@@ -8,7 +8,7 @@ export class ExhibitionAssembler {
       title: resource.title,
       short_description:resource.short_description,
       web_url: resource.web_url || '',
-      image_url: resource.image_url || '',
+      image_url: resource.image_url || 'https://firstbenefits.org/wp-content/uploads/2017/10/placeholder-1024x1024.png',
       status: resource.status,
       aic_start_at: resource.aic_start_at,
       aic_end_at: resource.aic_end_at,
