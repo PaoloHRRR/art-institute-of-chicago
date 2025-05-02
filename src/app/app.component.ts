f9faa6f91e3b235cd4eb842549facb09e96fb287
Henry Reaño <henry29.2004@hotmail.com>
2025-05-01 22:07:16 -0500

    feat: add footer component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,13 +4,15 @@ import {ExhibitionListComponent} from './exhibitions/components/exhibition-list/
 import {Exhibition} from './exhibitions/model/exhibition.entity';
 import {ExhibitionApiService} from './exhibitions/services/exhibition-api.service';
 import {ToolbarContentComponent} from './public/components/toolbar-content/toolbar-content.component';
+import {FooterContentComponent} from './public/components/footer-content/footer-content.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     ExhibitionListComponent,
-    ToolbarContentComponent
+    ToolbarContentComponent,
+    FooterContentComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
