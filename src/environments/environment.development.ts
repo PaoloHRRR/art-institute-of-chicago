88f776cdd9fedbf29d3e6127ee07613969cf3ed1
Henry Reaño <henry29.2004@hotmail.com>
2025-05-01 21:06:14 -0500

    bugfix: change exhibition endpoint path

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
   AICApiBaseUrl: 'https://api.artic.edu/api/v1',
-  exhibitionsEndpointPath: '/exhibitions?fields=id,title,short_description,status,web_url,image_url,gallery',
+  exhibitionsEndpointPath: '/exhibitions?fields=id,title,short_description,status,web_url,image_url,gallery_title,aic_start_at,aic_end_at&limit=25',
   logoProviderApiBaseUrl: 'https://logo.clearbit.com/'
 };
